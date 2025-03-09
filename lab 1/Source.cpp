d65ca27046d355d7e1971fe09e8a74e6f2d3d52d
anca0411 <anca.vasilica10@gmail.com>
2025-03-09 11:41:31 +0200

    SmallChange

--- lab 1/Source.cpp
+++ lab 1/Source.cpp
@@ -7,7 +7,7 @@ int main()
 	{
 		printf("Acesta este un numar: %d; Acesta este numarul 2: %d\n", test[i] - '0', 123);
 	}
-	cout << " Primul laborator OOP";
+	cout << " Primul laborator la OOP";
 
 	return 0;
 }
