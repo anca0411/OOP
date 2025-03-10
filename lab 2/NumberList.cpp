86742e41ea3fb7722d13a02799c189a37c0ad976
anca0411 <anca.vasilica10@gmail.com>
2025-03-10 11:24:39 +0200

    Laborator 2

--- lab 2/NumberList.cpp
+++ lab 2/NumberList.cpp
@@ -1,40 +1,32 @@
-// NumberList.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include "NumberList.h"
 
 #include <iostream>
-#include "NumberList.h"
-using namespace std;
+#define _SECURE_CRT_NO_WARNINGS
+
 void NumberList::Init()
 {
 	count = 0;
 }
 void NumberList::Add(int x)
 {
-	if (x < 10)
-	{
-		numbers[count] = x;
-		count = count + 1;
-	}
-	return false;
-
+	numbers[++count] = x;
 }
-void Sort()
+void NumberList::Sort()
 {
-	for (int i = 0; i < count-1; i++)
-	{
-		for (int j = i + 1; j < count; j++)
+	for(int i = 1; i< count; i++)
+		for (int j = i + 1; j <= count; j++)
+		{
 			if (numbers[i] > numbers[j])
 			{
 				int aux = numbers[i];
 				numbers[i] = numbers[j];
 				numbers[j] = aux;
 			}
-	}
+		}
 }
 void NumberList::Print()
 {
-	for (int i = 0; i < count; i++)
-		cout << numbers[i] << " ";
+	for (int i = 1; i <= count; i++)
+		std :: cout << numbers[i]<<" ";
+
 }
-
-
